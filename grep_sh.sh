044847a3884c7df778920121e3ab4acd736e8a80
narendrasinh3108 <58648916+narendrasinh3108@users.noreply.github.com>
2021-11-01 16:06:08 +0530

    Add files via upload

--- grep_sh.sh
+++ grep_sh.sh
@@ -6,10 +6,12 @@ echo -e $fname
 function seperator(){
    echo "###########################################"
 }
+
 function start(){
 	GREEN='\033[0;32m'
 	NC='\033[0m'
-        echo -e "${GREEN}}"
+	echo $1
+        echo -e "${GREEN}"
 	seperator
 	$1
 	seperator
@@ -23,6 +25,9 @@ do
       echo "2) Count the number of match string"
       echo "3) Search whole words in file"
       echo "4) Inverting the match string"
+      echo "5) Search multiple String"
+      echo "6) Print first N Number's line" 
+      echo "7) print last N Number's line"
       echo "*) Exit" 
       read -p "Choose: " ch
       
@@ -39,6 +44,31 @@ do
 	 4) read -p "Enter String: " str
 	    start "grep -v $str $fname"
 	    ;;
+	 5) str=''
+	    while true
+	    do
+              read -p "Enter String: " str1
+	      if [ -z $str ]
+              then
+		 str=$str1
+		 continue
+	      else 
+	      	 str="$str|$str1"
+	      fi
+	      read -p "add more y/n: " ans
+	      if [ $ans = 'n' ]
+	      then
+		  break
+	      fi
+            done
+	    start "grep -Ei $str $fname"
+	    ;;
+	 6) read -p "Enter N value: " val
+	    start "head -n $val $fname "
+	    ;;
+         7) read -p "Enter N value: " val
+	    start "tail -n $val $fname" 
+            ;;
          *) break ;;
       esac 
 done
